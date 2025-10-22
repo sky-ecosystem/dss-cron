4e1dec12d5aa246c3b5bd7a4e63eb8d634164cce
oldchili <130549691+oldchili@users.noreply.github.com>
2025-10-22 17:48:08 +0300

    Align FlapJob test, use KickerMock

---
+++ src/tests/mocks/KickerMock.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+// Copyright (C) 2025 Dai Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity ^0.8.13;
+
+contract KickerMock {
+    bool succeed;
+
+    function setSucceed(bool _succeed) external {
+        succeed = _succeed;
+    }
+
+    function flap() external view returns (uint256 id) {
+        if (succeed) return 0;
+        else revert();
+    }
+}
